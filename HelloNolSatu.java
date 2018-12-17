c2859e001ed3636c6692f5d6acbf2b6c776504f1
sitidy <pambudiono.os@gmail.com>
2018-12-17 14:57:48 +0700

    add HelloNolSatu

---
+++ HelloNolSatu.java
@@ -0,0 +1,8 @@
+public class NolSatu {
+  public static void main(String[] args) {
+    System.out.println("Hello, NolSatu");
+    System.out.println("This My First Java Project");
+    System.out.println("Sincerely");
+    System.out.println("Didiet A. Pambudiono");
+  }
+}
